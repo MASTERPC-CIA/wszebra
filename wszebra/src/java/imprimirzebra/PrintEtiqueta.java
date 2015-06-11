86c9664cc32e3be377d3770f424cebd0f71315e5
Mariuxi <mariuxikate@gmail.com>
2015-06-11 13:10:01 -0500

    1. Actualización: En formato de impresión en impresora Zebra.

--- wszebra/src/java/imprimirzebra/PrintEtiqueta.java
+++ wszebra/src/java/imprimirzebra/PrintEtiqueta.java
@@ -21,7 +21,7 @@ import javax.print.attribute.standard.PrinterName;
 public class PrintEtiqueta {
 
     /*Método que recibe como parametros los datos a imprimir en la etiqueta, a más del código de barras*/
-    public static void imprimirEtiq(String num_fact, String codigo, String name_provee, String date_compra, String name_impr) throws PrintException, FileNotFoundException, IOException {
+    public static void imprimirEtiq(String codigo, String name_produc, String precio_produc, String date_compra, String name_impr) throws PrintException, FileNotFoundException, IOException {
 
     // Se asigna en una variable el nombre de la impresora que se recibe como parámetro
         String printName = name_impr;
@@ -35,18 +35,18 @@ public class PrintEtiqueta {
         services = PrintServiceLookup.lookupPrintServices(null, aset);
 
     // Se crea las lineas de comandos que se envian a imprimir
-//        String zplCommand = "N\n"
-//                + "B220,40,0,1,2,4,100,N,\"test3\"\n"
-//                + "A220,150,0,1,1,1,N,\"\"+codigo+\"\n"
-//                + "P1\n";
+        String zplCommand = "N\n"
+                + "B20,280,0,1,2,4,100,N,\"test3\"\n"
+                + "A50,300,0,1,1,1,N,\"\"+codigo+\"\n"
+                + "P1\n";
 
-     String zplCommand = "^XA"   
-                        + "^FO300, 18^ADN, 11, 7^FD Nro. Factura "+ num_fact+"^FS"
-                        + "^FO320, 46^ADN, 11, 7^FD" +name_provee+"^FS   "
-                        //+ "^FO320, 74^ADN, 11, 7^FD "+date_compra+ " ^FS   "
-                        + "^FO230, 102^ADN, 11, 7 "
-                        + "^BCN, 60, Y, Y, N^FD >"+codigo+" ^FS "
-                        + "^XZ "; 
+//     String zplCommand = "^XA"   
+//                        + "^FO260, 18^ADN, 11, 7^FD Nro. Factura "+ date_compra+"^FS"
+////                        + "^FO230, 46^ADN, 11, 7 "
+//                        + "^BCN, 60, Y, Y, N^FD >"+codigo+" ^FS"
+//                        + "^FO300, 74^ADN, 11, 7^FD" +name_produc+"^FS"
+//                        + "^FO300, 102^ADN, 11, 7^FD "+precio_produc+ "^FS"
+//                        + "^XZ "; 
     // Se convierte el comando a bytes
         byte[] by = zplCommand.getBytes();
         DocFlavor flavor = DocFlavor.BYTE_ARRAY.AUTOSENSE;
--- wszebra/src/java/service/PrintZebra.java
+++ wszebra/src/java/service/PrintZebra.java
@@ -17,11 +17,9 @@ public class PrintZebra {
    
     /**
      * This is a sample web service operation
-     *
-     *
-     * @param num_fact
      * @param cod_produc
-     * @param name_provee
+     * @param name_produc
+     * @param precio_produc
      * @param date_compra
      * @param name_impr
      * @return 
@@ -30,13 +28,13 @@ public class PrintZebra {
      */
     @WebMethod(operationName = "print_zebra")
     public String print_zebra(
-            @WebParam(name = "num_fact") String num_fact,
             @WebParam(name = "cod_produc") String cod_produc,
-            @WebParam(name = "name_provee") String name_provee,
+            @WebParam(name = "name_produc") String name_produc,
+            @WebParam(name = "precio_produc") String precio_produc,
             @WebParam(name = "date_compra") String date_compra,
             @WebParam(name="name_impr") String name_impr) throws PrintException, IOException {
         // Se crea un objeto de la clase 
-        PrintEtiqueta.imprimirEtiq(num_fact, cod_produc, name_provee, date_compra, name_impr);
+        PrintEtiqueta.imprimirEtiq(cod_produc, name_produc, precio_produc, date_compra, name_impr);
         return "OK";
     }
 
